051ea72477a6c44089347c780f46a9bfab5bc6eb
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-03-14 18:41:52 +0100

    darts success

--- darts/darts.c
+++ darts/darts.c
@@ -1,9 +1,22 @@
 #include "darts.h"
 
+// formula : (px * px) + (py * py) <= r * r
 uint8_t score(coordinate_t position) 
 {
-    const int RADIUS = 10;
-    if(position.x == RADIUS && position.y <= RADIUS)
+    if(position.x == 0 && position.y == 0) return 10;
+
+    const float posX_Squared = position.x * position.x;
+    const float posY_Squared = position.y * position.y;
+    const float totalPos_squared = posX_Squared + posY_Squared;
+    if(totalPos_squared <= 1.F)
+    {
+        return 10;
+    }
+    else if (totalPos_squared <= 25.0F)
+    {
+        return 5;
+    }
+    else if(totalPos_squared <= 100.0F)
     {
         return 1;
     }
