1a9de8944bb13e758bd4c700746d571b03473f7f
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-04-10 19:11:59 +0200

    binary search

--- binary-search/binary_search.c
+++ binary-search/binary_search.c
@@ -1,6 +1,24 @@
 #include "binary_search.h"
 
 const int *binary_search(int value, const int *arr, size_t length)
-{
+{   
+    if (length == 0) return NULL;
+    if(length < 2) return &arr[0];
+    size_t middle = (size_t)length * 0.5;
+    if (value == arr[middle]) return &arr[middle];
+    if(value < arr[middle])
+    {
+        for(size_t i = 0; i < middle;++i)
+        {
+            if(value == arr[i]) return &arr[i];
+        }
+    }
+    else
+    {
+        for(size_t i = middle; i < length;++i)
+        {
+            if(value == arr[i]) return &arr[i];
+        }
+    }
     return NULL;
 }
