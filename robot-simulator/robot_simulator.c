28dd63462fc5719e0afa099f9bb5ad663c39443d
Manoloon <pablo.r.sanchez@hotmail.com>
2025-04-15 14:30:14 +0200

    robot

---
+++ robot-simulator/robot_simulator.c
@@ -0,0 +1,10 @@
+#include "robot_simulator.h"
+
+robot_status_t robot_create(robot_direction_t direction, int x, int y)
+{
+    return robot_status_t();
+}
+
+void robot_move(robot_status_t *robot, const char *commands)
+{
+}
