c40265fe27d6314d59db11ea623e3f75d85c9c19
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-05-06 07:14:33 +0200

    minesweeper

---
+++ minesweeper/minesweeper.c
@@ -0,0 +1,15 @@
+#include "minesweeper.h"
+
+char **annotate(const char **minefield, const size_t rows)
+{
+    int count =0;
+    for(size_t i = 0; i < rows;++i)
+    {
+
+    }
+    return nullptr;
+}
+
+void free_annotation(char **annotation)
+{
+}
