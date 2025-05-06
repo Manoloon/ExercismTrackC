9fead55f819ed5b30b67c0bf6cf3c0d83a81dafc
Manoloon <pablo.r.sanchez@hotmail.com>
2025-05-06 15:29:24 +0200

    mine

--- minesweeper/minesweeper.c
+++ minesweeper/minesweeper.c
@@ -1,15 +1,29 @@
 #include "minesweeper.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char **annotate(const char **minefield, const size_t rows)
 {
-    int count =0;
-    for(size_t i = 0; i < rows;++i)
+    int count = 0;
+    while (minefield[count] != NULL) count++;
+    char **result = malloc((count+1)* sizeof(char*));
+    if(!result) return NULL;
+    
+    for(size_t r = 0; r < rows;++r)
     {
-
+            if(minefield[r] == '.')
+            {
+                result[r] = strdup(minefield[r]);
+            }
     }
-    return nullptr;
+    // null-terminate the array
+    result[count] = NULL;
+
+    return result;
 }
 
 void free_annotation(char **annotation)
 {
+    free(annotation);
 }
