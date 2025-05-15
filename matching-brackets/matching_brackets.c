bf4e1d2f643b2519021b880698257000596a4850
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-05-15 17:53:09 +0200

    paired

--- matching-brackets/matching_brackets.c
+++ matching-brackets/matching_brackets.c
@@ -1,1 +1,20 @@
 #include "matching_brackets.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+bool is_paired(const char *input)
+{
+    if(input == NULL) return false;
+    size_t size = strlen(input);
+    printf("size %lld\n",size);
+    bool openBracket = false;
+    for(size_t i= 0; i < size; ++i)
+    {
+        if(input[i] == '[')
+            openBracket = true;
+        if(openBracket && input[i] == ']')
+            return true;
+    }
+    return false;
+}
