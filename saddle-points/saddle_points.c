7fbe7412a3ce2b3cedc473612befab327d6982ed
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-05-18 12:03:47 +0200

    saddle points

--- saddle-points/saddle_points.c
+++ saddle-points/saddle_points.c
@@ -1,1 +1,21 @@
 #include "saddle_points.h"
+
+saddle_points_t* saddle_points(int rows, int cols, uint8_t* grid)
+{
+    if(grid == NULL ) return NULL;
+
+    for(int r = 0; r < rows;++r)
+    {
+        for(int c = 0; c < cols;++c)
+        {
+            
+        }
+    }
+    
+    return NULL;
+}
+
+void free_saddle_points(saddle_points_t *points)
+{
+
+}
