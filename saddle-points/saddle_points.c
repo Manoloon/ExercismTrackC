0307e895dd4bf68d4da374faf7cb0b6ae68b767f
Manoloon <pablo.r.sanchez@hotmail.com>
2025-05-20 14:21:23 +0200

    advancing!

--- saddle-points/saddle_points.c
+++ saddle-points/saddle_points.c
@@ -3,7 +3,6 @@
 
 saddle_points_t* saddle_points(int rows, int cols, unsigned char grid[rows][cols])
 {
-    if(grid == NULL ) return NULL;
     saddle_points_t* result = malloc(sizeof(saddle_points_t));
     if(result == NULL) return NULL;
     result->points = malloc(rows * cols * sizeof(saddle_point_t));
@@ -16,39 +15,30 @@ saddle_points_t* saddle_points(int rows, int cols, unsigned char grid[rows][cols
     int count = 0;
     for(int r = 0; r < rows;r++)
     {
-        int minC = 0;
-        // find max in current row
+        int MaxCol = 0;
+        // largest in its row,
         for(int c =0; c < cols;c++)
         {
-            printf("min grid[%d][%d] : ",r,c);
-            printf("data : %d\n",grid[r][c]);
-            if(grid[r][c] > grid[r][minC])
+            if(grid[r][c] > grid[r][MaxCol])
             {
-                minC = c;
-                printf("minC = %d\n",minC);
+                MaxCol = c;
             }
         }
         int isSaddle = 1;
-        unsigned char best = grid[r][minC];
-        printf("best : %d in grid[%d][%d]\n",best,r,minC);
-        // check if this max is min in its column
+        unsigned char best = grid[r][MaxCol];
+        //  while being the smallest in its column
         for(int m = 0; m < rows;m++)
         {
-
-            printf("Max grid[%d][%d] : ",m,minC);
-            printf("data : %d\n",grid[m][minC]);
-            if(grid[m][minC] < best)
+            if(grid[m][MaxCol] < best)
             {
                 isSaddle = 0;
-                printf("bad grid[%d][%d]\n",m,minC);
                 break;
             }
         }
         if(isSaddle)
         {
-            result->points[count].row = r;
-            printf("Saddle: point[%d][%d]\n",r,minC);
-            result->points[count].column = minC;
+            result->points[count].row = r+1;
+            result->points[count].column = MaxCol+1;
             count++;
         }        
     }
