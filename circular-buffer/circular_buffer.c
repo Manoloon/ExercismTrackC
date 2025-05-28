ba326d8ab1c6f479326d7362a9d2134fe9518860
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-05-28 19:09:56 +0200

    circular buffer

---
+++ circular-buffer/circular_buffer.c
@@ -0,0 +1,30 @@
+#include "circular_buffer.h"
+#include <stdlib.h>
+
+circular_buffer_t *new_circular_buffer(size_t capacity)
+{
+    return NULL;
+}
+
+void delete_buffer(circular_buffer_t *buffer)
+{
+}
+
+void clear_buffer(circular_buffer_t *buffer)
+{
+}
+
+int16_t read(const circular_buffer_t *buffer_ptr, buffer_value_t *OutValue)
+{
+    return 0;
+}
+
+int16_t write(circular_buffer_t *buffer, buffer_value_t values)
+{
+    return 0;
+}
+
+int16_t overwrite(circular_buffer_t *buffer, buffer_value_t values)
+{
+    return 0;
+}
---
+++ circular-buffer/circular_buffer.h
@@ -0,0 +1,19 @@
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+#include <stdint.h>
+
+typedef uint8_t buffer_value_t;
+
+typedef struct circular_buffer_t 
+{
+    buffer_value_t* buffer;
+}circular_buffer_t;
+
+
+circular_buffer_t* new_circular_buffer(size_t capacity);
+void delete_buffer(circular_buffer_t* buffer);
+void clear_buffer(circular_buffer_t* buffer);
+int16_t read(const circular_buffer_t* buffer_ptr,buffer_value_t* OutValue);
+int16_t write(circular_buffer_t* buffer,buffer_value_t values);
+int16_t overwrite(circular_buffer_t* buffer,buffer_value_t values);
+#endif
