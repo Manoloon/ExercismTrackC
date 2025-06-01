b8fa13ac196c386faa473b9a366b1ec86ab1b000
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-06-01 19:04:00 +0200

    BST

--- binary-search-tree/binary_search_tree.c
+++ binary-search-tree/binary_search_tree.c
@@ -1,13 +1,34 @@
 #include "binary_search_tree.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 node_t *build_tree(int *tree_data, size_t tree_data_len)
 {
     if(tree_data == NULL) return NULL;
-    node_t* result = malloc(sizeof(node_t)*tree_data_len);
-    result->data = *tree_data;
-    result->left = NULL;
-    result->right = NULL;
+    node_t* result = malloc(sizeof(node_t));
+    if(result == NULL) return NULL;
+    printf("len %lld\n",tree_data_len);
+    if(tree_data_len < 2)
+    {
+        result->data = *tree_data;
+        result->left = NULL;
+        result->right = NULL;
+        return result;
+    }
+    for(size_t i = 0; i < tree_data_len;++i)
+    {
+        printf("data at %lld  = %d\n",i,tree_data[i]);
+            // left
+            if(tree_data[i] < tree_data[0])
+            {
+                result->left->data = tree_data[i];
+                result->right = NULL;
+            }
+            else
+            {
+                result->right->data = tree_data[i];
+                result->left = NULL;
+            }
+    }
     return result;
 }
 
